f08e400e30770bb8cfa06ce1ff1a60ccb0cc654e
Henrik Skovgaard <hnrk@skvgrd.com>
2023-05-23 16:03:36 +0200

    remove verbose

--- ScriptPad/METAL-PROD.ps1
+++ ScriptPad/METAL-PROD.ps1
@@ -130,8 +130,8 @@ Write-Host -ForegroundColor Green "Create C:\Windows\System32\OOBE.cmd"
 $OOBECMD = @'
 PowerShell -NoL -Com Set-ExecutionPolicy RemoteSigned -Force
 Set Path = %PATH%;C:\Program Files\WindowsPowerShell\Scripts
-Start /Wait PowerShell -NoL -C Install-Module AutopilotOOBE -Force -Verbose
-Start /Wait PowerShell -NoL -C Install-Module OSD -Force -Verbose
+Start /Wait PowerShell -NoL -C Install-Module AutopilotOOBE -Force
+Start /Wait PowerShell -NoL -C Install-Module OSD -Force
 Start /Wait PowerShell -NoL -C Invoke-WebPSScript https://raw.githubusercontent.com/woodyard/dsgr/main/oobe-METAL-PROD.ps1
 '@
 $OOBECMD | Out-File -FilePath 'C:\Windows\System32\OOBE.cmd' -Encoding ascii -Force

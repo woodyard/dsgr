9b9c75cccb5672ae5a124785e3a960a18beaf93e
Henrik Skovgaard <hnrk@skvgrd.com>
2023-06-09 13:28:06 +0200

    Update zti.ps1

--- zti.ps1
+++ zti.ps1
@@ -1,10 +1,4 @@
 Write-Host -ForegroundColor Green "Starting OSDCloud ZTI"
-Start-Sleep -Seconds 5
-
-#Make sure I have the latest OSD Content
-Write-Host -ForegroundColor Green "Updating OSD PowerShell Module"
-Install-Module OSD -Force
-
 Write-Host  -ForegroundColor Green "Importing OSD PowerShell Module"
 Import-Module OSD -Force
 

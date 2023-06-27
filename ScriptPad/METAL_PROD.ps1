76262c226a8aacf01199bf53d208d99ce622422a
Henrik Skovgaard <hnrk@skvgrd.com>
2023-06-27 10:43:43 +0200

    Update METAL_PROD.ps1

--- ScriptPad/METAL_PROD.ps1
+++ ScriptPad/METAL_PROD.ps1
@@ -132,7 +132,7 @@ PowerShell -NoL -Com Set-ExecutionPolicy RemoteSigned -Force
 Set Path = %PATH%;C:\Program Files\WindowsPowerShell\Scripts
 Start /Wait PowerShell -NoL -C Install-Module AutopilotOOBE -Force
 Start /Wait PowerShell -NoL -C Install-Module OSD -Force
-Start /Wait PowerShell -NoL -C Invoke-WebPSScript https://raw.githubusercontent.com/woodyard/dsgr/main/oobe-METAL-PROD.ps1
+Start /Wait PowerShell -NoL -C Invoke-WebPSScript https://raw.githubusercontent.com/woodyard/dsgr/main/oobe-METAL_PROD.ps1
 '@
 $OOBECMD | Out-File -FilePath 'C:\Windows\System32\OOBE.cmd' -Encoding ascii -Force
 
